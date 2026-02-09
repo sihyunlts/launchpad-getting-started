f12d914e27d2557a2d84ce602fa68d8fbfa3df03
Si Hyun Baek <62822977+sihyunlts@users.noreply.github.com>
2026-02-09 17:04:58 +0900

    Initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,27 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: {
+				ko: '런치패드 길라잡이',
+			},
+			defaultLocale: 'ko',
+			locales: {
+				ko: {
+					label: '한국어',
+					lang: 'ko',
+				},
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/sihyunlts/launchpad-getting-started' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: '처음부터 배우기',
+					collapsed: false,
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '시작하기', slug: 'getting-started/introduction' },
+						{ label: '설정하기', slug: 'getting-started/setup' },
+						{ label: 'Ableton Live 사용법', slug: 'getting-started/ableton-live' },
+						{ label: '프로젝트 연주', slug: 'getting-started/playing' },
 					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
